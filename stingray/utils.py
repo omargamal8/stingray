a718fffa7e2a0a06f968c79e5ba6ca3d1fc9af61
omargamal8 <omargamalh@hotmail.com>
2017-08-17 00:49:08 +0200

    implemented rebin using stingray.parallel

--- stingray/utils.py
+++ stingray/utils.py
@@ -106,30 +106,41 @@ def rebin_data(x, y, dx_new, yerr=None, method='sum', dx=None):
 
     step_size = dx_new / dx_old
 
-    output = []
-    outputerr = []
-    for i in np.arange(0, y.shape[0], step_size):
-        total = 0
-        totalerr = 0
+    intervals = np.arange(0,y.shape[0], step_size)
 
-        int_i = int(i)
-        prev_frac = int_i + 1 - i
-        prev_bin = int_i
-        total += prev_frac * y[prev_bin]
-        totalerr += prev_frac * (yerr[prev_bin]**2)
+    def wrapper(interval, pipe=None, index = 0):
+        output = []
+        outputerr = []
+        for i in interval:
+            total = 0
+            totalerr = 0
 
-        if i + step_size < len(x):
-            # Fractional part of next bin:
-            next_frac = i + step_size - int(i + step_size)
-            next_bin = int(i + step_size)
-            total += next_frac * y[next_bin]
-            totalerr += next_frac * (yerr[next_bin]**2)
+            int_i = int(i)
+            prev_frac = int_i + 1 - i
+            prev_bin = int_i
+            total += prev_frac * y[prev_bin]
+            totalerr += prev_frac * (yerr[prev_bin]**2)
 
-        total += sum(y[int(i+1):int(i+step_size)])
-        totalerr += sum(yerr[int(i+1):int(step_size)]**2)
-        output.append(total)
-        outputerr.append(np.sqrt(totalerr))
+            if i + step_size < len(x):
+                # Fractional part of next bin:
+                next_frac = i + step_size - int(i + step_size)
+                next_bin = int(i + step_size)
+                total += next_frac * y[next_bin]
+                totalerr += next_frac * (yerr[next_bin]**2)
 
+            total += sum(y[int(i+1):int(i+step_size)])
+            totalerr += sum(yerr[int(i+1):int(step_size)]**2)
+            output.append(total)
+            outputerr.append(np.sqrt(totalerr))
+
+        if pipe == None:
+            return output, outputerr
+        else:
+            pipe.send([output,outputerr])
+
+    from stingray.parallel import execute_parallel, _execute_sequential, post_concat_arrays
+    output, outputerr = execute_parallel(wrapper, [post_concat_arrays, post_concat_arrays], intervals, jit = False)
+    
     output = np.asarray(output)
     outputerr = np.asarray(outputerr)
 
