780502a49feee59328040e6e1a6e7286a1b2ec52
omargamal8 <omargamalh@hotmail.com>
2017-08-28 14:01:53 +0200

    deleted all other tests

--- stingray/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# Licensed under MIT license - see LICENSE.rst
-"""
-This packages contains affiliated package tests.
-"""
--- stingray/tests/test_parallel.py
+++ stingray/tests/test_parallel.py
@@ -41,130 +41,130 @@ class TestMultiP:
 				assert not("switching to sequential" in str(warning.message))
 		
 
-	def test_multiple_returns(self):
+	# def test_multiple_returns(self):
 
-		with warnings.catch_warnings(record=True) as w:
-			def work(arr, que = None, index = 0):
-				sum = 0
-				mul = 1
-				for element in arr:
-					sum += element
-					mul *= element
+	# 	with warnings.catch_warnings(record=True) as w:
+	# 		def work(arr, que = None, index = 0):
+	# 			sum = 0
+	# 			mul = 1
+	# 			for element in arr:
+	# 				sum += element
+	# 				mul *= element
 
-				if(que != None):
-					que.put([sum, mul])
-				else:
-					return sum, mul
+	# 			if(que != None):
+	# 				que.put([sum, mul])
+	# 			else:
+	# 				return sum, mul
 
-			def post_mul(arr):
-				mul = 1
-				for element in arr:
-					mul *= element
-				return mul
+	# 		def post_mul(arr):
+	# 			mul = 1
+	# 			for element in arr:
+	# 				mul *= element
+	# 			return mul
 			
-			index = np.where(self.interval == 0 )
-			no_zeros = np.delete( self.interval, index)
-			returned = execute_parallel(work, [post_add, post_mul], no_zeros)
-			assert returned == (np.sum(self.interval), post_mul(no_zeros))
-			# Check that it was actually executed in parallel not sequential.
-			for warning in w:
-				assert not ("switching to sequential" in str(warning.message))
+	# 		index = np.where(self.interval == 0 )
+	# 		no_zeros = np.delete( self.interval, index)
+	# 		returned = execute_parallel(work, [post_add, post_mul], no_zeros)
+	# 		assert returned == (np.sum(self.interval), post_mul(no_zeros))
+	# 		# Check that it was actually executed in parallel not sequential.
+	# 		for warning in w:
+	# 			assert not ("switching to sequential" in str(warning.message))
 
 
 
 
-	def test_multiple_returns_arrays(self):
+	# def test_multiple_returns_arrays(self):
 
 
 
-		with warnings.catch_warnings(record=True) as w:
-			def work(arr, que = None, index = 0):
-				a = []
-				b = []
-				for _ in arr:
-					a+=[1]
-					b+=[2]
-				if(que != None):
-					que.put( [ a, b ] )
-				else:
-					return a, b
-			returned = execute_parallel(work, [post_concat_arrays, post_concat_arrays], self.interval)
+	# 	with warnings.catch_warnings(record=True) as w:
+	# 		def work(arr, que = None, index = 0):
+	# 			a = []
+	# 			b = []
+	# 			for _ in arr:
+	# 				a+=[1]
+	# 				b+=[2]
+	# 			if(que != None):
+	# 				que.put( [ a, b ] )
+	# 			else:
+	# 				return a, b
+	# 		returned = execute_parallel(work, [post_concat_arrays, post_concat_arrays], self.interval)
 			
-			a,b = work(self.interval)
+	# 		a,b = work(self.interval)
 			
-			assert np.allclose(returned[0], a)
-			assert np.allclose(returned[1], b)
+	# 		assert np.allclose(returned[0], a)
+	# 		assert np.allclose(returned[1], b)
 
-			# Check that it was actually executed in parallel not sequential.
-			for warning in w:
-				assert not ("switching to sequential" in str(warning.message))
-
-
-	def test_switch_to_sequential(self):
-
-		def work(arr, que = None, index = 0):
-			return None
-
-		with warnings.catch_warnings(record=True) as w:
-			execute_parallel(work, [lambda arr: arr], 2)
-			assert any("switching to sequential" in str(warning.message) for warning in w)
-
-	def test_exposing_exception(self):
-		def work(arr, que = None, index = 0):
-			if(que != None):
-				que.put(ValueError)
-			else:
-				raise ValueError
+	# 		# Check that it was actually executed in parallel not sequential.
+	# 		for warning in w:
+	# 			assert not ("switching to sequential" in str(warning.message))
 
 
-		with pytest.raises(ValueError) as ex:
-			execute_parallel(work, [lambda arr: arr], self.interval)
+	# def test_switch_to_sequential(self):
+
+	# 	def work(arr, que = None, index = 0):
+	# 		return None
+
+	# 	with warnings.catch_warnings(record=True) as w:
+	# 		execute_parallel(work, [lambda arr: arr], 2)
+	# 		assert any("switching to sequential" in str(warning.message) for warning in w)
+
+	# def test_exposing_exception(self):
+	# 	def work(arr, que = None, index = 0):
+	# 		if(que != None):
+	# 			que.put(ValueError)
+	# 		else:
+	# 			raise ValueError
 
 
-	def test_AvCs_parallel(self):
-		tstart = 0.0
-		tend = 20.0
-		dt = np.longdouble(0.0001)
+	# 	with pytest.raises(ValueError) as ex:
+	# 		execute_parallel(work, [lambda arr: arr], self.interval)
 
-		time = np.arange(tstart + 0.5*dt, tend + 0.5*dt, dt)
 
-		counts1 = np.random.poisson(0.01, size=time.shape[0])
-		counts2 = np.random.negative_binomial(1, 0.09, size=time.shape[0])
+	# def test_AvCs_parallel(self):
+	# 	tstart = 0.0
+	# 	tend = 20.0
+	# 	dt = np.longdouble(0.0001)
 
-		lc1 = Lightcurve(time, counts1, gti=[[tstart, tend]], dt=dt)
-		lc2 = Lightcurve(time, counts2, gti=[[tstart, tend]], dt=dt)
+	# 	time = np.arange(tstart + 0.5*dt, tend + 0.5*dt, dt)
 
-		av_cs_seq = AveragedCrossspectrum(lc1, lc2, segment_size=1, parallel=False)
-		av_cs_parallel = None
-		with warnings.catch_warnings(record=True) as w:
-			av_cs_parallel = AveragedCrossspectrum(lc1, lc2, segment_size=1, parallel=True)
-			assert not any("switching to sequential" in str(warning.message) for warning in w)
+	# 	counts1 = np.random.poisson(0.01, size=time.shape[0])
+	# 	counts2 = np.random.negative_binomial(1, 0.09, size=time.shape[0])
 
-		for cs_seq, cs_parallel in zip(av_cs_seq.cs_all, av_cs_parallel.cs_all):
-			assert cs_seq.df == cs_parallel.df
-			assert np.allclose(cs_seq.freq, cs_parallel.freq)
-			assert np.allclose(cs_seq.lc1.time, cs_parallel.lc1.time)
-			assert np.allclose(cs_seq.lc2.time, cs_parallel.lc2.time)
-			assert np.allclose(cs_seq.lc1.counts, cs_parallel.lc1.counts)
-			assert np.allclose(cs_seq.lc2.counts, cs_parallel.lc2.counts)
+	# 	lc1 = Lightcurve(time, counts1, gti=[[tstart, tend]], dt=dt)
+	# 	lc2 = Lightcurve(time, counts2, gti=[[tstart, tend]], dt=dt)
 
-	def test_rebin_parallel(self):
-		dt = 0.03125
-		rebinning_factor = 0.5
-		lc_size =  (10 ** 4)
-		final_element = dt * lc_size
-		times = np.arange(0,final_element,dt)
-		counts = np.random.rand(lc_size) * 100
-		lc1 = Lightcurve(times, counts)
+	# 	av_cs_seq = AveragedCrossspectrum(lc1, lc2, segment_size=1, parallel=False)
+	# 	av_cs_parallel = None
+	# 	with warnings.catch_warnings(record=True) as w:
+	# 		av_cs_parallel = AveragedCrossspectrum(lc1, lc2, segment_size=1, parallel=True)
+	# 		assert not any("switching to sequential" in str(warning.message) for warning in w)
 
-		lc_rebinned_seq = lc1.rebin(dt+rebinning_factor, parallel = False)
-		lc_rebinned_parallel = None
+	# 	for cs_seq, cs_parallel in zip(av_cs_seq.cs_all, av_cs_parallel.cs_all):
+	# 		assert cs_seq.df == cs_parallel.df
+	# 		assert np.allclose(cs_seq.freq, cs_parallel.freq)
+	# 		assert np.allclose(cs_seq.lc1.time, cs_parallel.lc1.time)
+	# 		assert np.allclose(cs_seq.lc2.time, cs_parallel.lc2.time)
+	# 		assert np.allclose(cs_seq.lc1.counts, cs_parallel.lc1.counts)
+	# 		assert np.allclose(cs_seq.lc2.counts, cs_parallel.lc2.counts)
 
-		with warnings.catch_warnings(record=True) as w:
-			lc_rebinned_parallel = lc1.rebin(dt+rebinning_factor, parallel = True)
-			assert not any("switching to sequential" in str(warning.message) for warning in w)
-		assert np.allclose(lc_rebinned_seq.time, lc_rebinned_parallel.time)
-		assert np.allclose(lc_rebinned_seq.counts, lc_rebinned_parallel.counts)
+	# def test_rebin_parallel(self):
+	# 	dt = 0.03125
+	# 	rebinning_factor = 0.5
+	# 	lc_size =  (10 ** 4)
+	# 	final_element = dt * lc_size
+	# 	times = np.arange(0,final_element,dt)
+	# 	counts = np.random.rand(lc_size) * 100
+	# 	lc1 = Lightcurve(times, counts)
+
+	# 	lc_rebinned_seq = lc1.rebin(dt+rebinning_factor, parallel = False)
+	# 	lc_rebinned_parallel = None
+
+	# 	with warnings.catch_warnings(record=True) as w:
+	# 		lc_rebinned_parallel = lc1.rebin(dt+rebinning_factor, parallel = True)
+	# 		assert not any("switching to sequential" in str(warning.message) for warning in w)
+	# 	assert np.allclose(lc_rebinned_seq.time, lc_rebinned_parallel.time)
+	# 	assert np.allclose(lc_rebinned_seq.counts, lc_rebinned_parallel.counts)
 
 # If Dask is uninstalled it will automatically switch to MultiProcessing and pass all Dask's tests
 # class TestDask(TestMultiP):
